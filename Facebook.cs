cc63ab5209474237b6a4f841dd861ea5e8b0af8b
GonzaRom <rom.gonzalo.i@gmail.com>
2020-07-03 01:10:56 -0300

    Lets do it

---
+++ Facebook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array_and_List
+{
+    public static class Facebook
+    {
+        // If no one likes your post, it doesn't display anything.
+        // If only one person likes your post, it displays: [Friend's Name] likes your post.
+        // If two people like your post, it displays: [Friend 1] and[Friend 2] like your post.
+        //If more than two people like your post, it displays: [Friend 1], [Friend 2] and[Number of Other People] others like your post.
+        public static string CheckLikability(int count, List<string> namesList)
+        {
+            if ((namesList == null) || (count > namesList.Count)) return " ";
+
+            else
+            {
+                switch (count)
+                {
+                    case 1:
+                        return namesList[0] + " likes your post.";
+                    case 2:
+                        return String.Format("{0}, {1} likes your post", namesList[0], namesList[1]);
+                    default:
+                    {
+                        if (count > 2) {
+                            return String.Format("{0}, {1} and {2} others like your post.", namesList[0], namesList[1],
+                                count - 2);
+                        }
+                        else return "";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Array_and_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var names = new List<String>();
+
+            var count = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Roses are red, violets are blue …");
+                Console.WriteLine(Facebook.CheckLikability(count,names));
+                
+                Console.WriteLine("\n\nEnter a name or just press ENTER to finish:");
+                var name = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(name)) break;
+                names.Add(name);
+                count++;
+            }
+
+        }
+    }
+}
